1128a0c8830044e9dc4ae5610917c4532041a684
Dylan Smith <dylantsmith@gmail.com>
2013-09-16 11:01:00 -0500

    add documentation to the BooksControllerTest

--- src/test/java/org/library/web/BooksControllerTest.java
+++ src/test/java/org/library/web/BooksControllerTest.java
@@ -31,13 +31,28 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+/**
+ * A test of the {@link BooksController} that's more of an integration test than a unit test,
+ * because of it's use of spring-test to load up the context prior to running the test. This test
+ * focuses on the "/books" REST API endpoint.
+ * 
+ * @author dylants
+ * 
+ */
+// This annotation is required to load up a test context and take advantage of the
+// many Spring test resources. In the end, it's still a JUnit test, but this annotation
+// links up Spring's test context without having to use Spring's JUnit classes.
 @RunWith(SpringJUnit4ClassRunner.class)
+// Informs Spring to load up these configuration classes prior to running the test
 @ContextConfiguration(classes = {WebApplicationInitializer.RootContextConfiguration.class,
         WebApplicationInitializer.ServletContextConfiguration.class})
+// Specifies the active profile for the configuration classes
 @ActiveProfiles("development")
+// Specifies that the application context should be a WebApplicationContext
 @WebAppConfiguration
 public class BooksControllerTest {
 
+    // Used to convert JSON to Java, and Java to JSON
     private static ObjectMapper objectMapper;
 
     @Autowired
@@ -45,6 +60,7 @@ public class BooksControllerTest {
 
     @Autowired
     private BooksController bookController;
+
     private MockMvc mockMvc;
 
     @BeforeClass
@@ -54,9 +70,16 @@ public class BooksControllerTest {
 
     @Before
     public void setupBefore() {
+        // Create our MockMvc utilizing the current WebApplicationContext. This will give
+        // us access to all the endpoints configured within our application.
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
+    /**
+     * Tests the GET action for all {@link Book}s
+     * 
+     * @throws Exception
+     */
     @Test
     public void testGET_AllBooks() throws Exception {
         ResultActions actions = this.mockMvc.perform(get("/books"));
@@ -76,6 +99,12 @@ public class BooksControllerTest {
         Assert.assertTrue("there must be at least one book", books.size() > 0);
     }
 
+    /**
+     * Tests the GET action for a single {@link Book} that was preloaded in the database prior to
+     * running any tests (in the "development" profile)
+     * 
+     * @throws Exception
+     */
     @Test
     public void testGET_SingleBook() throws Exception {
         // perform the GET request
@@ -98,6 +127,11 @@ public class BooksControllerTest {
         Assert.assertEquals("author must match", "Brian Greene", book.getAuthor());
     }
 
+    /**
+     * Tests the error case of GET on a single {@link Book} that does not exist
+     * 
+     * @throws Exception
+     */
     @Test
     public void testGET_BookThatDoesNotExist() throws Exception {
         String bookId = "99999999";
@@ -108,6 +142,11 @@ public class BooksControllerTest {
         actions.andExpect(status().isNotFound());
     }
 
+    /**
+     * Tests the POST action to create a {@link Book}
+     * 
+     * @throws Exception
+     */
     @Test
     public void testPOST_CreateABook() throws Exception {
         String title = "Jamberry";
@@ -136,6 +175,11 @@ public class BooksControllerTest {
         Assert.assertEquals("author must match", author, createdBook.getAuthor());
     }
 
+    /**
+     * Tests the PUT action by first creating a {@link Book}, and then updating this {@link Book}
+     * 
+     * @throws Exception
+     */
     @Test
     public void testPUT_CreateAndUpdateBook() throws Exception {
         String title = "The Cat in the Hat";
@@ -180,6 +224,12 @@ public class BooksControllerTest {
         Assert.assertEquals("author must match", createdBook.getAuthor(), updatedBook.getAuthor());
     }
 
+    /**
+     * Tests the error case of the PUT action by attempting to update a {@link Book} that does not
+     * exist.
+     * 
+     * @throws Exception
+     */
     @Test
     public void testPUT_UpdateBookThatDoesNotExist() throws Exception {
         String title = "The Big Hungry Bear";
@@ -197,6 +247,12 @@ public class BooksControllerTest {
         actions.andExpect(status().is(404));
     }
 
+    /**
+     * Tests the DELETE action by creating a {@link Book} and then deleting it, verifying it is no
+     * longer there.
+     * 
+     * @throws Exception
+     */
     @Test
     public void testDELETE_CreateAndDeleteBook() throws Exception {
         String title = "Hug";
@@ -232,6 +288,11 @@ public class BooksControllerTest {
         actions.andExpect(status().is(404));
     }
 
+    /**
+     * Tests the error case of DELETE by attempting to delete a {@link Book} that does not exist
+     * 
+     * @throws Exception
+     */
     @Test
     public void testDELETE_DeleteBookThatDoesNotExist() throws Exception {
         // perform the DELETE request
@@ -242,6 +303,15 @@ public class BooksControllerTest {
         actions.andExpect(status().is(404));
     }
 
+    /**
+     * Internal method that converts a JSON {@link String} of a {@link Book} to a {@link Book}
+     * object
+     * 
+     * @param content
+     *            A {@link Book} in JSON {@link String} format
+     * @return The {@link Book} object
+     * @throws Exception
+     */
     protected Book convertToBook(String content) throws Exception {
         // hopefully temporary, but convert to Object to remove links, then to Book
         @SuppressWarnings("unchecked")
@@ -251,6 +321,14 @@ public class BooksControllerTest {
         return book;
     }
 
+    /**
+     * Internal method that converts a JSON list of {@link Book}s into {@link Book} objects
+     * 
+     * @param content
+     *            A list of {@link Book}s in JSON {@link String} format
+     * @return The {@link List} of {@link Book}s
+     * @throws Exception
+     */
     protected List<Book> convertToBooks(String content) throws Exception {
         // hopefully temporary, but pull just the content to convert to books
         @SuppressWarnings("unchecked")
